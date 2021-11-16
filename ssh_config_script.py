2118d1b3694468295951626df8ee4fb6acf73adb
kraumar <kraumar.com@gmail.com>
2021-11-16 23:14:18 +0100

    adding python script to create config ssh file

---
+++ ssh_config_script.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+os.remove("/home/marek-ubu/Documents/IAC/config.tmp")
+
+file = open("/home/marek-ubu/Documents/IAC/slave-public-ips")
+
+ips = []
+
+for line in file:
+	ips.append(line.strip())
+
+n_lines = len(ips)
+
+file = open("config.tmp", "a")
+
+namenode = """Host nnode
+  HostName 89.64.44.23
+  User hadoop
+  IdentityFile ~/.ssh/id_rsa"""
+
+file.write(namenode + '\n')
+
+
+for i in range(n_lines):
+	datanode = """Host dnode""" + str(i + 1) +"""
+  HostName """ + ips[i] + """
+  User hadoop
+  IdentityFile ~/.ssh/id_rsa"""
+	file.write('\n' + datanode + '\n')
+
+os.replace("/home/marek-ubu/Documents/IAC/config.tmp","/home/marek-ubu/.ssh/config")
+
