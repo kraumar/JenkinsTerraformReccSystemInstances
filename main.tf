66b91dcbd13b92e167a34245064831056b822955
kraumar <kraumar.com@gmail.com>
2021-11-16 20:52:16 +0100

    fixes

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "aws_instance" "slave-node-1a" {
 	}
 
         provisioner "local-exec" {
-                command = "echo ${aws_instance.web.public_ip} >> /home/marek-ubu/Documents/IAC/slave-public-ips.txt"
+                command = "echo ${aws_instance.slave-node-1a.public_ip} >> /home/marek-ubu/Documents/IAC/slave-public-ips.txt"
         }
 
 	tags = {
@@ -139,7 +139,7 @@ resource "aws_instance" "slave-node-1b" {
         }
 
 	provisioner "local-exec" {
-		command = "echo ${aws_instance.web.public_ip} >> /home/marek-ubu/Documents/IAC/slave-public-ips.txt"
+		command = "echo ${aws_instance.slave-node-1b.public_ip} >> /home/marek-ubu/Documents/IAC/slave-public-ips.txt"
 	}
 
         tags = {
@@ -167,7 +167,7 @@ resource "aws_instance" "slave-node-1c" {
         }
 
         provisioner "local-exec" {
-                command = "echo ${aws_instance.web.public_ip} >> /home/marek-ubu/Documents/IAC/slave-public-ips.txt"
+                command = "echo ${aws_instance.slave-node-1c.public_ip} >> /home/marek-ubu/Documents/IAC/slave-public-ips.txt"
         }
 
         tags = {
