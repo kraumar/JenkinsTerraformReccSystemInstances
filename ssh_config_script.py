d38d2da7274f83db5e15bf6ee7d7c3440f0416dd
kraumar <kraumar.com@gmail.com>
2021-11-21 16:34:30 +0100

    changing user

--- ssh_config_script.py
+++ ssh_config_script.py
@@ -15,8 +15,8 @@ n_lines = len(ips)
 file = open("./config.tmp", "a")
 
 namenode = """Host nnode
-  HostName 89.64.44.23
-  User marek-ubu
+  HostName 18.193.117.36
+  User ubuntu
   IdentityFile ~/.ssh/id_rsa"""
 
 file.write(namenode + '\n')
@@ -25,7 +25,7 @@ file.write(namenode + '\n')
 for i in range(n_lines):
 	datanode = """Host dnode""" + str(i + 1) +"""
   HostName """ + ips[i] + """
-  User marek-ubu
+  User ubuntu
   IdentityFile ~/.ssh/id_rsa"""
 	file.write('\n' + datanode + '\n')
 
