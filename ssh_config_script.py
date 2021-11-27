850297c6543ed6783d6d1979069bea1956591e47
kraumar <kraumar.com@gmail.com>
2021-11-27 14:35:26 +0100

    changing addr

--- ssh_config_script.py
+++ ssh_config_script.py
@@ -15,7 +15,7 @@ n_lines = len(dns)
 file = open("./config.tmp", "a")
 
 namenode = """Host nnode
-  HostName ec2-3-67-194-50.eu-central-1.compute.amazonaws.com
+  HostName ec2-3-68-149-109.eu-central-1.compute.amazonaws.com
   User hadoop
   IdentityFile ~/.ssh/id_rsa"""
 
