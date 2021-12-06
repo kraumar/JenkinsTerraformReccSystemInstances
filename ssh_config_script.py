f4ae428316bb755584aa8d703055c1cad83318fc
kraumar <kraumar.com@gmail.com>
2021-12-06 16:07:00 +0100

    x

--- ssh_config_script.py
+++ ssh_config_script.py
@@ -15,7 +15,7 @@ n_lines = len(dns)
 file = open("./config.tmp", "a")
 
 namenode = """Host nnode
-  HostName ec2-3-122-243-232.eu-central-1.compute.amazonaws.com
+  HostName ec2-3-70-25-76.eu-central-1.compute.amazonaws.com
   User hadoop
   IdentityFile ~/.ssh/id_rsa"""
 
